c590d90bd132df75df12ea4298b201f3c276571d
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2021-12-10 22:32:25 +0000

    add badge next to github PR and issues

--- generate-book.sh
+++ generate-book.sh
@@ -2,13 +2,29 @@
 
 set -e
 
-if [ ! -d src ]; then
-    mkdir src
-fi
+add_badge() {
+  TITLE=${1:-PR}
+  TYPE=${2:-pull}
+  SHIELD_IO_TYPE=${3:-$TYPE}
+  
+  echo "$TITLE - $TYPE - $SHIELD_IO_TYPE"
+
+  FIND="\(https://github.com/(.*)/(.*)/$TYPE/(.*)\)"
+  SUBS="(https:\/\/github.com\/\1\/\2\/$TYPE\/\3) ![$TITLE \3 badge](https:\/\/shields.io\/github\/$SHIELD_IO_TYPE\/detail\/state\/\1\/\2\/\3)"
+
+  sed -ibak -E "s|$FIND|$SUBS|g" src/*-*.md
+}
+
+rm -rf book/ src/
+mkdir src
 
 printf '[Introduction](introduction.md)\n\n' > src/SUMMARY.md
 
 find ./text ! -type d -print0 | xargs -0 -I {} ln -frs {} -t src/
+
+# add badge next to github issues & PRs
+add_badge "Issues" "issues"
+add_badge "PR" "pull" "pulls"
 
 find ./text ! -type d -name '*.md' -print0 \
   | sort -z \
